33100c166577298e0d128beb9c0feb5147eb9a96
Hassan Aarish <aarishhassan98@gmail.com>
2024-10-02 18:58:32 +0500

    Changes.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -12,38 +12,22 @@ const Table = () => {
     }))
   );
   const [currentIndex, setCurrentIndex] = useState(0);
-
-  const disabled = () => {};
+  // const [admin, setAdmin] = useState("null");e
 
   const handleDescisionChange = (value, index) => {
-    setDataState((prevDataState) =>
-      prevDataState.map((row, i) => {
-        if (i === index) {
-          if (value === "P") {
-            return {
-              ...row,
-              decisionValue: value,
-              banker: "NP",
-              player: 1,
-              opposite: "NP",
-              sameOpposite: "NP",
-              plusMinus: "-",
-            };
-          } else if (value === "B") {
-            return {
-              ...row,
-              decisionValue: value,
-              banker: 1,
-              player: "NP",
-              opposite: "NP",
-              sameOpposite: "NP",
-              plusMinus: "",
-            };
-          }
-        }
-        return row;
-      })
-    );
+    setCurrentIndex(index + 1);
+    setDataState((prevDataState) => {
+      const newArr = prevDataState;
+      newArr[index] = {
+        decisionValue: value,
+        banker: value === "B" ? 1 : "NP",
+        player: value === "P" ? 1 : "NP",
+        opposite: "NP",
+        sameOpposite: "NP",
+        plusMinus: "",
+      };
+      return newArr;
+    });
   };
 
   return (
@@ -62,6 +46,7 @@ const Table = () => {
             <select
               onChange={(e) => handleDescisionChange(e.target.value, i)}
               value={dt.decisionValue}
+              disabled={currentIndex !== i}
             >
               <option value="">Select Value</option>
               <option value="B">B</option>
